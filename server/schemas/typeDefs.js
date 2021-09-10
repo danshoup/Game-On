d9fa1f73d282544f7bc6f3b0adc233efbcf51887
Diana Daghlas <dianadaghlas@yahoo.com>
2021-09-09 21:37:18 -0400

    Creating typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,41 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+     _id: ID!
+    name: String!
+    email: String!
+    skills: String!
+  }
 
+  type Category {
+      _id: ID!
+      name: String!
+  }
+
+  type Competition {
+      _id: ID!
+      name: String!
+      location: String!
+      organizer: String!
+      challenged: String!
+      date: Date
+      victor: String!
+      organizerScore: Decimal128
+      challengedScore: Decimal 128
+      category: String!
+  }
+
+  type Query {
+    user: [User]
+    category: [Category]
+    competition(_id: ID): [Competition]
+  }
+
+  type Mutation {
+    createCompetition(name: String!, location: String!): Competition
+    createStats(_id: ID!, statValue: Decimal128): Competition
+  }
 `;
 
 module.exports = typeDefs;
