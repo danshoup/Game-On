fdb011c2634fe808298c68c12a5d49940b6f3ef3
Diana Daghlas <dianadaghlas@yahoo.com>
2021-09-09 22:02:47 -0400

    Create resolver code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,30 @@
-const { Profile } = require('../models');
+const { User, Category, Competition } = require('../models');
 
+// Need to incorporate User model within query
 const resolvers = {
+    Query: {
+        Category: async () => {
+          return Category.find({});
+        },
+        Competition: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return Competition.find(params);
+      },
+    },
+    Mutation: {
+      createCompetition: async (parent, args) => {
+        const competition = await Competition.create(args);
+        return competition;
+      },
+      createScore: async (parent, { _id, statValueNum }) => {
+        const score = await Competition.findOneAndUpdate(
+          { _id },
+          { $inc: { [`user${statValueNum}_score`]: 1 } },
+          { new: true }
+        );
+        return score;
+      },
+    },
   
 };
 
