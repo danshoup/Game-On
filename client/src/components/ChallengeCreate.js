c9468eef13053027175462f5b622f61e1f9af8d5
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-09-11 09:29:43 -0400

    reviewed react client files, added router to route components, and added needed component files

---
+++ client/src/components/ChallengeCreate.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ChallengeCreate extends Component {
+  render() {
+    return <h1>Welcome To Game On</h1>
+  }
+}
+
+export default ChallengeCreate
+
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class HomePage extends Component {
   render() {
-    return <h1>Welcome To Game On</h1>
+    return <h1>Welcome To Game On; This is the Main Info Page</h1>
   }
 }
 
