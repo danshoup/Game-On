77d6cd2bdf907215c354333bddf690f77f114680
Diana Daghlas <dianadaghlas@yahoo.com>
2021-09-12 18:48:11 -0400

    Create mutations in Client/Src/Utils for creating competition, score and stats

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,38 @@
 import { gql } from '@apollo/client';
+
+export const CREATE_COMPETITION = gql`
+  mutation createCompetition($_id: String!, $name: String!, $location: String!, $category: String! $organizer: String!, $challenged: String!, $date: Date!) {
+    createCompetition(_id: $_id, name: $name, location: $location, category: $category, organizer: $organizer, challenged: $challenged, date: $date) {
+      _id
+      name
+      location
+      category
+      organizer
+      challenged
+      date
+    }
+  }
+`;
+
+export const CREATE_SCORE = gql`
+  mutation createScore($_id: String!, $victor: String!, $organizerScore: Decimal128!, $challengedScore: String!, $category: String!, #status: String!) {
+    createScore(_id: $_id, victor: $victor, organizerScore: $organizerScore, challengedScore: $challengedScore, category: $category, status: $status) {
+      _id
+     victor
+     organizerScore
+     challengedScore
+     category
+     status
+    }
+  }
+`;
+
+export const CREATE_STATS = gql`
+  mutation createStats($_id: String!, $statName: String!, $statValue: String!) {
+      createStats(_id: $_id, statName: $statName, statValue: $statValue) {
+          _id
+          statName
+          statValue
+      }
+  }
+  `;
