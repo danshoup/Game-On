a0ab6416af41e4c056da52ea7141709f8bf83880
Diana Daghlas <dianadaghlas@yahoo.com>
2021-09-13 20:46:17 -0400

    Removed create_score

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -55,19 +55,6 @@ export const CONFIRM_COMPETITION = gql`
   }
 `;
 
-export const CREATE_SCORE = gql`
-  mutation createScore($_id: String!, $victor: String!, $organizerScore: Decimal128!, $challengedScore: String!, $category: String!, #status: String!) {
-    createScore(_id: $_id, victor: $victor, organizerScore: $organizerScore, challengedScore: $challengedScore, category: $category, status: $status) {
-      _id
-     victor
-     organizerScore
-     challengedScore
-     category
-     status
-    }
-  }
-`;
-
 export const CREATE_STATS = gql`
   mutation createStats($_id: String!, $statName: String!, $statValue: String!) {
       createStats(_id: $_id, statName: $statName, statValue: $statValue) {
