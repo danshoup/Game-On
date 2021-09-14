39f9157a9b953eeb8fecef89755be2ae971a4860
Olja Priyakovich <oljap.@oljas-air.attlocal.net>
2021-09-13 21:38:52 -0400

    signupform

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
-
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 class SignUp extends Component {
   render() {
-    return <h1>Sign Up Here</h1>
+    return (
+      <Form className="signUp-form">
+        <h1 className="font-weight-bold text-center">Game On
+        </h1>
+        <FormGroup>
+          <Label>Full Name</Label>
+          <Input type="name" placeholder="Full Name"/>
+        </FormGroup>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input type="email" placeholder="Email"/>
+        </FormGroup>
+        <FormGroup>
+          <Label>Zip Code</Label>
+          <Input type="text" placeholder="Zip Code"/>
+        </FormGroup>
+        <FormGroup>
+          <Label>Password</Label>
+          <Input type="password" placeholder="Password"/>
+        </FormGroup>
+        <FormGroup>
+          <Label>Confirm Password</Label>
+          <Input type="password" placeholder="Password"/>
+        </FormGroup> 
+        <Button className="btn-lg btn-dark btn-block">
+          Sign Up
+        </Button>
+
+      </Form>
+
+    );
   }
 }
 
-export default SignUp
-
+export default SignUp;
