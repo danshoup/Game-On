399b8431d74e0993a92900632e101131adb543dc
Olja Priyakovich <oljap.@oljas-air.attlocal.net>
2021-09-15 08:51:36 -0400

    fixing form layout

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -4,39 +4,32 @@ class SignUp extends Component {
   render() {
     return (
       <Form className="signUp-form">
-        <h1 className="font-weight-bold text-center">Matchup
+        <h1 className="font-weight-bold text-center">Game On
         </h1>
         <h6>
-          IAN H. VS. DANIEL S.
+         Create an Account 
         </h6>
         <Form.Group>
           <Form.Label></Form.Label>
-          <FormControl type="name" placeholder="Sports"/>
+          <FormControl type="name" placeholder="Full Name"/>
         </Form.Group>
         <Form.Group>
           <Form.Label></Form.Label>
-          <FormControl type="email" placeholder="Pool"/>
+          <FormControl type="email" placeholder="Email"/>
         </Form.Group>
         <Form.Group>
           <Form.Label></Form.Label>
-          <FormControl type="text" placeholder="8-Ball"/>
+          <FormControl type="text" placeholder="Zip Code"/>
         </Form.Group>
-        <Form.Group>
-          <Form.Label></Form.Label>
-          <FormControl type="text" placeholder="Billiards"/>
+        <Form.Group className="mb-3"  controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <Form.Group>
-        <Form.Label for="exampleSearch"></Form.Label>
-          <FormControl
-          type="search"
-          name="search"
-          id="exampleSearch"
-          placeholder="search"
-          />
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <Form.Group>
-          <Form.Label></Form.Label>
-        </Form.Group>
+
         <div className="d-grid gap-2">
         <Button variant="dark" size="lg">
           Create Matchup
