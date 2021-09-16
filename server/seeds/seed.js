e36d186d4dddf83f63e5d1d890dfd7f8d3ea3a4d
Diana Daghlas <dianadaghlas@yahoo.com>
2021-09-16 17:48:13 -0400

    Refactored seed.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,13 +1,31 @@
 const db = require('../config/connection');
-const { User } = require('../models');
-
+const { User, Category, Competition } = require('../models');
 const userData = require('./userData.json');
+const categoryData = require('./categoryData.json');
+const competitionData = require('./competitionData.json');
 
 db.once('open', async () => {
-  await User.deleteMany({});
+  await Category.deleteMany();
 
-  const users = await User.insertMany(userData);
+  const categories = await Category.insertMany([
+    {categoryData}
+  ]);
 
-  console.log('Users seeded!');
-  process.exit(0);
+  console.log(categories + "seeded");
+
+await Competition.deleteMany();
+
+const competitions = await Competition.insertMany([
+  {competitionData}
+]);
+
+console.log(competitions + "seeded");
+
+await User.deleteMany();
+
+await User.create({userData});
+
+console.log('users seeded');
+
+process.exit();
 });
